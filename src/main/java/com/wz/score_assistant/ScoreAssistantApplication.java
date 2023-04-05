be7c7fb6ae34b0d0d9fb5b7338a5d83b4bc0e268
qzx0712 <1500418656@qq.com>
2023-04-05 23:25:48 +0800

    添加定时任务访问数据库,防止数据库连接中断

--- src/main/java/com/wz/score_assistant/ScoreAssistantApplication.java
+++ src/main/java/com/wz/score_assistant/ScoreAssistantApplication.java
@@ -2,7 +2,10 @@ package com.wz.score_assistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling
 public class ScoreAssistantApplication {
 
     public static void main(String[] args) {
